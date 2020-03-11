14b30a9e6eb913485d6ce48b835d8347737023eb
mammonny <54335870+mammonny@users.noreply.github.com>
2020-03-11 18:09:36 +0100

    Update Doc.vue

--- src/templates/Doc.vue
+++ src/templates/Doc.vue
@@ -23,9 +23,9 @@ query Doc ($path: String!) {
 export default {
   metaInfo() {
     return {
-      title: this.$page.doc.meta-title,
+      title: $page.doc.meta-title,
       meta: [
-        { key: 'description', name: 'description', content: this.$page.doc.description }
+        { key: 'description', name: 'description', content: $page.doc.meta-description }
       ]
     }
   }
